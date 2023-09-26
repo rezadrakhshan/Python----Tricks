143bd58b39cb5035c4d85374d156021221f37831
RezaDerakhshan <130975344+rezadrakhshan@users.noreply.github.com>
2023-09-26 19:43:45 +0330

    Add new trick

---
+++ Trick(7)/README.md
@@ -0,0 +1,3 @@
+<h1>Output</h1>
+<br>
+<p>Counter({'apple': 2, 'benana': 2, 'cherry': 1})</p>
---
+++ Trick(7)/code.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+fruits = ["apple","benana","apple","benana","cherry"]
+
+fruit_count = Counter(fruits)
+
+print(fruit_count)

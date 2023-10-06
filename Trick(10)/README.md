d53c11908c3cae6fff1b2d1bb880cf298924a1c6
RezaDerakhshan <130975344+rezadrakhshan@users.noreply.github.com>
2023-10-06 18:39:35 +0330

    Add Trick 10

---
+++ Trick(10)/README.md
@@ -0,0 +1,8 @@
+<h1>Output</h1>
+<br>
+<pre>
+    github.com
+    youtube.com       
+    google.com        
+    linkedin.com 
+</pre>
---
+++ Trick(10)/code.py
@@ -0,0 +1,9 @@
+links = [
+    "www.github.com",
+    "www.youtube.com",
+    "www.google.com",
+    "www.linkedin.com"
+]
+
+for link in links:
+    print(link.removeprefix("www."))

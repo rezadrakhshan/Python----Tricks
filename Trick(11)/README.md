499736bd7686eee1973697be6bcec409517de781
RezaDerakhshan <130975344+rezadrakhshan@users.noreply.github.com>
2023-10-06 18:48:06 +0330

    Add Trick 11

---
+++ Trick(11)/README.md
@@ -0,0 +1,8 @@
+<h1>OutPut</h1>
+<br>
+<pre>
+    0 Learning Django
+    1 Brush Teeth
+    2 Training   
+    3 Go To Gym  
+</pre>
---
+++ Trick(11)/code.py
@@ -0,0 +1,8 @@
+todo_list = [
+    'learning django',
+    'brush teeth',
+    'training',
+    'go to gym'
+]
+for i , thing in enumerate(todo_list):
+    print(i, thing.title())
